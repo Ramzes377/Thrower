89ef867478fedd7e7434967cd2d7ebd731ddc965
Baranov_DR <mrramzes37@gmail.com>
2022-09-28 16:16:56 +0300

    Update db tables. Change db data execution style. Replace some functionality between modules.

--- api/cogs/commands.py
+++ api/cogs/commands.py
@@ -170,10 +170,9 @@ class Commands(BaseCog):
     async def game_statistics(self, before, after):
         if user_is_playing(after):
             app_id, _ = get_app_id(after)
-            gamerole_time = await self.get_gamerole_time(after.id, app_id)
-            if not gamerole_time or (gamerole_time and not gamerole_time[0]):
-                await self.execute_sql(f"""INSERT INTO UserActivities (role_id, user_id, seconds) VALUES 
-                                        ((SELECT role_id from CreatedRoles where app_id = {app_id}), {after.id}, {0})""")
+            await self.execute_sql(f"""INSERT INTO UserActivities (role_id, user_id, seconds) VALUES 
+                                    ((SELECT role_id from CreatedRoles where app_id = {app_id}), {after.id}, {0})
+                                            ON CONFLICT (role_id) DO NOTHING""")
         if user_is_playing(before):
             app_id, _ = get_app_id(before)
             gamerole_time = await self.get_gamerole_time(before.id, app_id)
