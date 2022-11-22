7cbae92784f3bc517685fa47e0ff8bdb4990130d
Baranov_DR <mrramzes37@gmail.com>
2022-11-22 14:06:51 +0300

    restructure project add detail logger first realization

---
+++ api/core/logger/log_detail.py
@@ -0,0 +1,122 @@
+import datetime
+import sqlite3
+
+connection = sqlite3.connect('logger-details.db')
+cursor = connection.cursor()
+
+create_tables_query = [
+    """CREATE TABLE IF NOT EXISTS DetailedLog
+                            (
+                                message_id bigint NOT NULL,
+                                PRIMARY KEY (message_id)
+                            )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS Leadership
+        (
+            message_id bigint NOT NULL,
+            user_id bigint NOT NULL,
+            begin timestamp without time zone NOT NULL,
+            end timestamp without time zone DEFAULT NULL,
+            FOREIGN KEY (message_id) REFERENCES DetailedLog(message_id) ON DELETE CASCADE
+        )
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS Activities
+        (
+            message_id bigint NOT NULL,
+            activity_id bigint NOT NULL,
+            begin timestamp without time zone NOT NULL,
+            end timestamp without time zone DEFAULT NULL,
+            PRIMARY KEY (message_id, activity_id, begin),
+            FOREIGN KEY (message_id) REFERENCES DetailedLog(message_id) ON DELETE CASCADE
+        )
+    """,
+    """
+         CREATE TABLE IF NOT EXISTS MemberPresence
+         (
+             message_id bigint NOT NULL,
+             member_id bigint NOT NULL,
+             begin timestamp without time zone NOT NULL,
+             end timestamp without time zone DEFAULT NULL,
+             FOREIGN KEY (message_id) REFERENCES DetailedLog(message_id) ON DELETE CASCADE
+         )
+     """
+]
+
+for query in create_tables_query:
+    cursor.execute(query)
+
+connection.commit()
+
+
+def register_detailed_log(message_id: int):
+    cursor.execute(f"""INSERT INTO DetailedLog (message_id) VALUES ({message_id}) ON CONFLICT DO NOTHING""")
+    connection.commit()
+
+
+def get_detailed_msgs():
+    cursor.execute(f"""SELECT message_id from DetailedLog""")
+    return cursor.fetchall()
+
+
+def detailed_log_activity(message_id: int, activity_id: int, begin: datetime.datetime = datetime.datetime.now(),
+                          end: datetime.datetime = None):
+    if end is None:
+        try:
+            cursor.execute(f"""INSERT INTO Activities (message_id, activity_id, begin, end) 
+                                    VALUES ({message_id}, {activity_id}, '{begin.strftime('%Y-%m-%d %H:%M:%S')}', null)""")
+        except sqlite3.IntegrityError:
+            pass
+    else:
+        try:
+            cursor.execute(
+                f"""UPDATE Activities SET activity_id = {activity_id}, end = '{end.strftime('%Y-%m-%d %H:%M:%S')}'
+                                    WHERE message_id = {message_id} and activity_id = {activity_id} and end is NULL""")
+        except sqlite3.IntegrityError:
+            pass
+    connection.commit()
+
+
+def leadership_begin(message_id: int, leader_id: int, begin: datetime.datetime = datetime.datetime.now()):
+    cursor.execute(f"""INSERT INTO Leadership (message_id, user_id, begin, end) 
+                            VALUES ({message_id}, {leader_id}, '{begin.strftime('%Y-%m-%d %H:%M:%S')}', null)""")
+    connection.commit()
+
+
+def leadership_end(message_id: int, leader_id: int, _time: datetime.datetime, new_leader_id: int = None):
+    cursor.execute(f"""UPDATE Leadership SET end = '{_time.strftime('%Y-%m-%d %H:%M:%S')}'
+                            WHERE message_id = {message_id} and user_id = {leader_id}""")
+    if new_leader_id is not None:
+        leadership_begin(message_id, new_leader_id, _time)
+    connection.commit()
+
+
+def member_join(message_id: int, member_id: int, begin: datetime.datetime):
+    print('CALLING MEMBER JOIN')
+    cursor.execute(f"""INSERT INTO MemberPresence (message_id, member_id, begin, end) 
+                            VALUES ({message_id}, {member_id}, '{begin.strftime('%Y-%m-%d %H:%M:%S')}', null)""")
+    connection.commit()
+
+
+def member_leave(message_id: int, member_id: int, end: datetime.datetime):
+    cursor.execute(f"""UPDATE MemberPresence SET end = '{end.strftime('%Y-%m-%d %H:%M:%S')}'
+                            WHERE message_id = {message_id} and member_id = {member_id} and end is NULL""")
+    connection.commit()
+
+
+def get_leaders_list(message_id: int) -> list[tuple[int, datetime.datetime, datetime.datetime]]:
+    cursor.execute(f"""SELECT user_id, begin, end FROM Leadership WHERE message_id = {message_id} ORDER BY begin""")
+    return cursor.fetchall()
+
+
+def get_prescence_list(message_id: int) -> list[tuple[int, datetime.datetime, datetime.datetime]]:
+    cursor.execute(
+        f"""SELECT member_id, begin, end FROM MemberPresence WHERE message_id = {message_id} ORDER BY end""")
+    return cursor.fetchall()
+
+
+def get_activities_list(message_id: int) -> list[tuple[int, datetime.datetime, datetime.datetime]]:
+    cursor.execute(
+        f"""SELECT activity_id, begin, end FROM Activities WHERE message_id = {message_id} ORDER BY end""")
+    return cursor.fetchall()
---
+++ api/core/logger/views.py
@@ -0,0 +1,64 @@
+from typing import Callable, Generator
+
+import discord
+
+from api.core.logger.log_detail import get_leaders_list, get_activities_list, get_prescence_list
+
+from api.tools.mixins import ConnectionMixin
+from api.tools.utils import fmt, dt_from_str
+
+
+def format_date(s: str) -> str:
+    return fmt(dt_from_str(s))
+
+
+def format_members(getter: Callable, message_id: int) -> Generator:
+    return ((f"<@{l[0]}>", format_date(l[1]), format_date(l[2]) if l[2] is not None else '\u200b' * 10 + "-") for l in
+            getter(message_id))
+
+
+def create_embed(title: str, first_column: str):
+    embed = discord.Embed(title=title)
+    embed.add_field(name=first_column, value='\u200b', inline=True)
+    embed.add_field(name="Время начала", value='\u200b', inline=True)
+    embed.add_field(name="Время окончания", value='\u200b', inline=True)
+    return embed
+
+
+def set_embed_row(embed, *row):
+    for field in row:
+        embed.add_field(name='\u200b', value=field, inline=True)
+
+
+class LoggerView(discord.ui.View, ConnectionMixin):
+    def __init__(self, bot) -> None:
+        super().__init__(timeout=None)
+        self.bot = bot
+
+    async def format_activities(self, message_id: int) -> str:
+        res = []
+        for id, begin, end in get_activities_list(message_id):
+            role_id = await self.execute_sql(f"SELECT role_id FROM CreatedRoles WHERE app_id = {id}")
+            res.append((f'<@&{role_id}>' if role_id else 'Deleted role', format_date(begin), format_date(end)))
+        return res
+
+    @discord.ui.button(style=discord.ButtonStyle.primary, emoji="👑", custom_id='logger_view:leadership')
+    async def leadership(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        embed = create_embed(title='Лидеры сессии', first_column='Лидер')
+        for member_id, begin_date, end_date in format_members(get_leaders_list, interaction.message.id):
+            set_embed_row(embed, member_id, begin_date, end_date)
+        await interaction.response.send_message(embed=embed, ephemeral=False, delete_after=30)
+
+    @discord.ui.button(style=discord.ButtonStyle.blurple, emoji="🎮", custom_id='logger_view:activities')
+    async def activities(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        embed = create_embed(title='Активности сессии', first_column='Активность')
+        for activity_id, begin_date, end_date in await self.format_activities(interaction.message.id):
+            set_embed_row(embed, activity_id, begin_date, end_date)
+        await interaction.response.send_message(embed=embed, ephemeral=False, delete_after=30)
+
+    @discord.ui.button(style=discord.ButtonStyle.blurple, emoji="🚶", custom_id='logger_view:prescence')
+    async def prescence(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        embed = create_embed(title='Участники сессии', first_column='Участник')
+        for member_id, begin_date, end_date in format_members(get_prescence_list, interaction.message.id):
+            set_embed_row(embed, member_id, begin_date, end_date)
+        await interaction.response.send_message(embed=embed, ephemeral=False, delete_after=30)
