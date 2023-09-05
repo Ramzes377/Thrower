41b63df4c53bce19118408dcb8f2778057a40bdb
Baranov_DR <mrramzes37@gmail.com>
2023-09-05 20:55:56 +0300

    rest API access uvicorn dependency

--- bot/api/rest/base.py
+++ bot/api/rest/base.py
@@ -2,7 +2,7 @@ import fastapi
 from fastapi.encoders import jsonable_encoder
 from httpx import AsyncClient
 
-from .v1 import router
+from bot.api.rest.v1 import router
 
 base = "http://127.0.0.1:8000"
 api_ver = "v1"
@@ -18,14 +18,14 @@ async def request(
         data: dict | None = None,
         params: dict | None = None
 ) -> dict:
-
     async with AsyncClient(app=app, base_url=base_url) as client:
 
         if method in ('get', 'delete'):
             response = await client.request(method, url, params=params)
         else:
             data = jsonable_encoder(data)
-            response = await client.request(method, url, params=params, json=data)
+            response = await client.request(method, url, params=params,
+                                            json=data)
 
     return response.json()
 
@@ -38,4 +38,9 @@ def home():
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('api.rest.base:app', host='127.0.0.1', port=8000, reload=True)
+    uvicorn.run(
+        app='bot.api.rest.base:app',
+        host='127.0.0.1',
+        port=8000,
+        reload=True
+    )
